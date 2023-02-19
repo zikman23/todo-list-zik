e22a08e6162a0a202c93076039069de0502bbab7
Ahmad Karimi <zikman@gmail.com>
2023-02-18 19:37:50 -0800

    Refactor form

--- index.js
+++ index.js
@@ -13,31 +13,26 @@ function App() {
       isCompleted: false,
     },
   ]);
-  const [value, setValue] = React.useState('');
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-    const newTodos = [...todos, { text: value, isCompleted: false }];
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text: text, isCompleted: false }];
     setTodos(newTodos);
-    setValue('');
+  };
+
+  const removeTodo = (e) => {
+    const index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
   };
   return (
     <>
       {todos.map((todo, i) => (
-        <div className="todo" key={i}>
+        <div className="todo" key={i} id={i} onClick={removeTodo}>
           {todo.text}
         </div>
       ))}
-      <form onSubmit={handleSubmit}>
-        {' '}
-        <input
-          type="text"
-          className="input"
-          value={value}
-          placeholder="Add todo..."
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </form>
+      <TodoForm addTodo={addTodo} />
     </>
   );
 }
